d51bed7331e73153427c4047062f1d1bb01262f5
Sarah <sarahborretti@gmail.com>
2019-11-19 15:30:53 -0300

    Lista de Motivação do Robson (todas as questões feitas)

--- listas de motivação/ListaMot4.py
+++ listas de motivação/ListaMot4.py
@@ -1,6 +1,6 @@
 #LISTA DE MOTIVAÇÃO 4
 
-#1 - soma de a, b e c
+#1- soma de a, b e c
 
 #solução com vetor
 def soma(num):
@@ -20,8 +20,6 @@ for i in range(3):
     n.append(int(input("numero: ")))
 
 print("\na soma é:", soma(n))            
-
-#==--**--==#
 
 #solução sem vetor
 def soma(a, b, c):
@@ -46,7 +44,7 @@ print(soma(a, b, c))
 ######################################
 
 
-#2 - alunos e médias
+#2- alunos e médias
 n = int(input("número de alunos: "))
 
 nome = []
@@ -103,7 +101,7 @@ print("percentual de reprovados: ", (rep / n) * 100, "%")
 ###########################
 
 
-#3 - area do circulo
+#3- area do circulo
 def area(r):
     return 3.141516*(r**2)
 #
@@ -114,7 +112,7 @@ print("a área é:", area(r))
 ###########################
 
 
-#4 - numeros primos
+#4- numeros primos
 def primo():
     for n in range(1, 100):
         d = 1
@@ -143,4 +141,141 @@ print(reverso(n))
 
 #############################
 
-#6- 
+
+#6- 100
+
+
+#7- diferença
+def dif(x, y):
+    if(x < y):
+        return y - x
+    else:
+	return x - y
+		
+x = int(input("x: "))
+y = int(input("y: "))
+
+print(dif(x, y))
+	
+
+#############################
+
+
+#8- 13 / 17 / 4
+
+
+#9- 5 4 3 2 1 0 1 2 3 4 5
+
+
+#############################
+
+
+#10- triângulo de pascal
+import numpy as np
+
+def fat(n):
+    fat = 1
+    if(n != 0):
+        for i in range(1, n+1):
+            fat = fat * i
+
+    return fat
+
+def comb(m, n):
+    comb = fat(m)/(fat(n) * fat(m-n))
+    return comb
+
+
+n = int(input("valor do numerador: "))
+p = int(input("valor do denominador: "))
+
+for i in range(n+1):
+    tri = []
+    for j in range(i+1):
+        tri.append(comb(i, j))
+    print(tri)
+
+
+#############################
+
+
+#11
+def ad():
+    a = float(input("primeiro valor: "))
+    b = float(input("segundo valor: "))
+	
+    soma = a + b
+	
+    if(soma > 20):
+	print(soma + 8)
+    else:
+	print(soma - 5)
+		
+ad()
+
+
+#############################
+
+
+#12
+for i in range(1000,10000):
+    a = str(i)
+    pr = int(a[0] + a[1])
+    se = int(a[2] + a[3])
+	
+    prova = (pr + se)**2
+	
+    if(prova == i):
+	print(i, "-->", pr, "+", se, "=", pr + se, "--> (", pr + se, ")^2 =", prova)
+
+
+#############################
+	
+	
+#13
+def exp(n, m):
+    soma = 0
+	
+    for i in range(2, n):
+	 for j in range(3, m, 2):
+	    soma = soma + (i/j)
+			
+    return soma
+	
+n = 0
+
+while(n <= 0):
+    n = int(input("n (positivo): "))
+	
+m = int(input("m: "))
+
+print(exp(n, m))
+
+
+#############################
+
+
+#14 - sistemas de equações
+import numpy as np
+
+#-2x + 4y - z = 5
+#3x - 2y + 4z = 3
+#-x + 5y - 4z = 3
+
+a = np.matrix ([
+        [-2, 4, -1],
+        [3, -2, 4],
+        [-1, 5, -4]
+        ])
+
+b = np.matrix ([
+        [5],
+        [3],
+        [3]
+        ])
+
+
+aIn = np.linalg.inv(a)
+print(aIn)
+
+print("solução:\n", aIn * b)
